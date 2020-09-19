a24f18cb5bd8723a882c2269418eb0283dbceeda
EgorKaravaev <you@example.com>
2020-09-19 20:50:21 +0500

    Save

--- database.php
+++ database.php
@@ -20,10 +20,3 @@ $pdo -> exec("CREATE TABLE users (
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ");
 
-//$pdo -> exec("CREATE TABLE users_info (
-//                        id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Индивидуальный номер записи',
-//                        user_id BIGINT UNSIGNED NOT NULL,
-//                        info TEXT NOT NULL,
-//                        PRIMARY KEY (id)
-//    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-//");
